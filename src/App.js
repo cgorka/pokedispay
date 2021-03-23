5f993b8c2f9cf0bd2ef8c1b0e50258cdab56ae4e
cgorka <cgorka@gmail.com>
2021-03-23 14:04:01 +0100

    snorlax

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
-import React, {useState,useEffect} from 'react';
-import './App.css';
-import 'react-medium-image-zoom/dist/styles.css'
-import pokemon from 'pokemontcgsdk'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "react-medium-image-zoom/dist/styles.css";
+import pokemon from "pokemontcgsdk";
 import Header from "./Header";
 import Pokemon from "./Pokemon";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
+  const [pocemons, setPocemon] = useState([]);
+  pokemon.configure({ apiKey: "03da6d84-de02-4d7f-babf-1358bfcbfd8c" });
+  useEffect(() => {
+    pokemon.card
+      // .where({ q: "set.id:base1", orderBy: "number" })
+      .where({ q: "name:snorlax", orderBy: "number" })
+      .then((card) => {
+        console.log(card); // "Charizard"
+        console.log(card.data);
+        setPocemon(card.data);
+      });
+  }, []);
 
-    const [pocemons, setPocemon] = useState([]);
-    pokemon.configure({apiKey: '03da6d84-de02-4d7f-babf-1358bfcbfd8c'})
-    useEffect(
-        () => {
-            pokemon.card.where({ q: 'set.id:base1',orderBy:'number' }).then((card) => {
-
-                console.log(card) // "Charizard"
-                console.log(card.data)
-                setPocemon(card.data);
-            })
-
-        }, []
-    );
-
-
-// pokemon.card.find('base1-4')
-//     .then(card => {
-//         console.log(card.name) // "Charizard"
-//         console.log('card.images.small: ', card.images.small);
-//         console.log('card.images.large: ', card.images.large);
-//         setPocemon(card.images)
-//     })
-
-return (
-    <div className="App">
+  return (
+    <BrowserRouter>
+      <div className="App">
         <Header></Header>
-        {pocemons.length>0 ? (
-             pocemons.map(pokemon=>(
-                 <Pokemon pokemon={pokemon}></Pokemon>
-             ))
-
-
+        {pocemons.length > 0 ? (
+          pocemons.map((pokemon) => <Pokemon pokemon={pokemon}></Pokemon>)
         ) : (
-            <div></div>
+          <div></div>
         )}
-
-    </div>
-);
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -5,14 +5,14 @@ const Header = (props) => {
     return (
         <header className="header">
             <div className="topBar">
-                <a href="http://proxycroak.com" className="topBar__logo">
+                <a href="#" className="topBar__logo">
                     <img id="mainLogo" width='350px'
                          src={mainLogo}
                          alt="Logo">
                     </img></a>
                 <nav className="topBar__nav">
                     <ul className="topBar__nav__list">
-                        <li className="topBar__nav__item"><a href="/features">Features</a></li>
+                        <li className="topBar__nav__item"><a href="/series">Series</a></li>
                         <li className="topBar__nav__item"><a href="/changelog">Changelog</a></li>
                         <li className="topBar__nav__item"><a href="/help">Help</a></li>
                         <li className="topBar__nav__item"><a href="https://ko-fi.com/A814HDT"
